05608faae0cd5f8430d837b6cc8fd090e68c1814
LiaSav <liasav19@ukr.net>
2023-08-30 23:19:21 +0300

    add layout for 1600px

---
+++ js/index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // lang
+    const langCurrent = document.querySelector('.header__lang-current'),
+        langMenu = document.querySelector('.header__lang-menu'),
+        langItems = document.querySelectorAll('.header__lang-menu-item'),
+        langCurrentImg = document.querySelector('#header__lang-current-img');
+
+
+    langCurrent.addEventListener('click', () => {
+        langMenu.classList.add('header__lang-menu_active');
+    });
+
+    for (let i = 0; i < langItems.length; i++) {
+        let langItem = langItems[i];
+        langItem.addEventListener('click', changeLanguage);
+    }
+
+    function changeLanguage() {
+        langCurrentImg.src = '../images/langs/' + this.dataset.lang + '.svg';
+        langMenu.classList.remove('header__lang-menu_active');
+    }
+
+    // matches
+    const matchesTabs = document.querySelectorAll('.matches__tab'),
+        matchesItemLinks = document.querySelectorAll('.matches__item-link');
+
+    matchesTabs.forEach(button => {
+        button.addEventListener('click', () => {
+            const sideBar = button.parentElement,
+                tabsContainer = sideBar.parentElement,
+                tabName = button.dataset.forTab,
+                tabToActive = tabsContainer.querySelector(`.matches__content[data-tab="${tabName}"]`);
+
+            matchesTabs.forEach(tab => {
+                tab.classList.remove('matches__tab_active');
+            });
+
+            tabsContainer.querySelectorAll('.matches__content').forEach(content => {
+                content.classList.remove('matches__content_active');
+            });
+
+            button.classList.add('matches__tab_active');
+            tabToActive.classList.add('matches__content_active');
+        });
+    });
+
+
+    matchesItemLinks.forEach(item => {
+        item.addEventListener('click', () => {
+            item.nextElementSibling.classList.toggle('matches__submenu_active');
+
+            if (item.nextElementSibling.classList.contains('matches__submenu_active')) {
+                item.lastElementChild.lastElementChild.src = './images/matches-flags/arrow-active.png';
+            } else {
+                item.lastElementChild.lastElementChild.src = './images/matches-flags/arrow.png';
+            }
+
+
+        })
+    })
+});
